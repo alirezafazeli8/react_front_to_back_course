5457161afeba6db0319bbb2ad10ad4f48bb33357
alireza fazeli <alirezafazeli2684@gmail.com>
2022-02-12 21:35:34 +0330

    for deploy test

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Header(props) {
-	const styles = { backgroundColor: props.bgColor, color: props.textColor };
-	return (
-		<header style={styles}>
-			<div className="container">
-				<h1>{props.title}</h1>
-			</div>
-		</header>
-	);
+    const styles = {backgroundColor: props.bgColor, color: props.textColor};
+    return (
+        <header style={styles}>
+            <div className="container">
+                <h1>{props.title}</h1>
+            </div>
+        </header>
+    );
 }
 
 // we set default props
 Header.defaultProps = {
-	title: 'Feedback UI',
-	bgColor: 'rgba(0,0,0,0.4)',
-	textColor: '#ff6a95',
+    title: 'Feedback UI - test',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
 };
 
 // prop type
 Header.propTypes = {
-	title: PropTypes.string,
+    title: PropTypes.string,
 };
 
 export default Header;
