06130b32fbf981d205aa5f03730950c126b5c3cb
alireza fazeli <alirezafazeli2684@gmail.com>
2022-04-10 21:12:25 +0430

    practice dynamic lists and conditional component on react

--- new codes/feedback/src/App.js
+++ new codes/feedback/src/App.js
@@ -1,10 +1,49 @@
 import react from 'react';
-// import reactDOM from 'react-dom';
 
 function App() {
+	const title = 'Blog Post';
+	const body = 'this is my body in the blog post';
+	const comment = [
+		{
+			id: 1,
+			text: 'this is comment from here',
+		},
+		{
+			id: 2,
+			text: 'this is comment from here',
+		},
+		{
+			id: 3,
+			text: 'this is comment from here',
+		},
+		{
+			id: 4,
+			text: 'this is comment from here',
+		},
+	];
+	const isLoading = false;
+
+	// is loading true
+	if (isLoading) return <h1>Loading ...</h1>;
+
+	const showComment = true;
+	const commentCodeBlock = (
+		<div>
+			<h2>comment ({comment.length})</h2>
+			<ul>
+				{comment.map((value, i) => {
+					return <li key={i}>{value.text}</li>;
+				})}
+			</ul>
+		</div>
+	);
+
 	return (
-		<div className="container">
-			<h1>im in app</h1>
+		<div>
+			<h1>{title}</h1>
+			<h3>{body}</h3>
+			{/* show comment */}
+			{showComment && commentCodeBlock}
 		</div>
 	);
 }
