99fe98e28fd71866467407539fac0bfc5faea85f
alireza fazeli <alirezafazeli2684@gmail.com>
2022-04-10 19:29:48 +0430

    feedback app code is initialed for start

---
+++ new codes/feedback/src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+// import reactDOM from 'react-dom';
+
+function App() {
+	return (
+		<div className="container">
+			<h1>im in app</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ new codes/feedback/src/index.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+reactDOM.render(
+	<react.StrictMode>
+		<App />
+	</react.StrictMode>,
+	document.getElementById('root')
+);
