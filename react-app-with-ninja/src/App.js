2deec946d4503dbaf676a58e6d103f7c288c6683
alirezafazeli <alirezafazeli34@gmail.com>
2022-05-28 10:41:42 +0430

    now we know about the how jsx is work 🍙

--- react-app-with-ninja/src/App.js
+++ react-app-with-ninja/src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import { useState } from 'react';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-	const [name, setName] = useState('Default Name');
+	const [name, setName] = useState("Default Name");
 
 	const handleClick = () => {
 		// console.log(name);
-		setName('name changed');
+		setName("name changed");
 	};
+
+	// if we write jsx - in fact we do this
+	// const newElement = React.createElement("div", { className: "App" }, "Hello");
 
 	return (
 		<div className="App">
 			<p>{name}</p>
 			<button onClick={handleClick}>change name</button>
+			{/* {newElement} */}
 		</div>
 	);
 }
